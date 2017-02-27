6b595a4c12ffb97bb0890e72916b90c55a21fc7c
Timothy Chen <timothy.chen01@gmail.com>
2017-02-27 14:50:33 -0500

    Add access control header fix

--- server.js
+++ server.js
@@ -16,6 +16,21 @@ mongoose.connection.on('error', () => {
   process.exit();
 });
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 
 // mongoose.connect('mongodb://localhost/food');
 let Menu = mongoose.model('Menu', {
